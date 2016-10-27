361379b1f28eec013672497803de27389ee13510
sun peng <sp900409@gmail.com>
2016-10-26 22:17:46 -0700

    add login valid

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
 from flask  import Flask
 
+from src.common.database import Database
+
 app = Flask(__name__)
 app.config.from_object('config')
+app.secret_key = "123"
 
 
 @app.route('/')
 def hello_world():
     return "Hello world"
 
+@app.before_first_request
+def init_db():
+    Database.initialize()
+
+from src.models.users.views import user_blueprint
+app.register_blueprint(user_blueprint, url_prefix="/users")
--- src/models/users/views.py
+++ src/models/users/views.py
@@ -1,11 +1,30 @@
 from flask import Blueprint
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import session
 
+from src.models.users.user import User
 
 user_blueprint = Blueprint('User', __name__)
 
-@user_blueprint.route('/login')
+@user_blueprint.route('/login', methods=['GET', 'POST'])
 def login_user():
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['hashed']
+
+        if User.is_login_valid(email, password):
+            session['email'] = email
+            return redirect(url_for(".user_alerts"))
+        # return None
+
+    return render_template("users/login.html")  #Send user a error if their login was invalid
+
+
+
+
 
 
 @user_blueprint.route('/register')
@@ -15,12 +34,12 @@ def reister_user():
 
 @user_blueprint.route('/alerts')
 def user_alerts():
-    pass
+    return "This is the alert page"
 
 @user_blueprint.route('/logout')
 def logout_user():
     pass
 
-@user_blueprint.route('/check_alerts/<string:user_id')
+@user_blueprint.route('/check_alerts/<string:user_id>')
 def check_for_alert(user_id):
     pass
